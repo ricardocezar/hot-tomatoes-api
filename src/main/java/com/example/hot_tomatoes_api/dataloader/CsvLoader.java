e6120e3d7f7313fe2faab580c3f30cd71bf557da
Ricardo Dias <dias.ricardocezar@gmail.com>
2024-06-02 18:47:26 -0300

    moving csv filepath to application properties

--- src/main/java/com/example/hot_tomatoes_api/dataloader/CsvLoader.java
+++ src/main/java/com/example/hot_tomatoes_api/dataloader/CsvLoader.java
@@ -1,6 +1,7 @@
 package com.example.hot_tomatoes_api.dataloader;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import java.util.List;
@@ -11,11 +12,12 @@ import java.util.logging.Logger;
 public class CsvLoader implements CommandLineRunner {
     Logger logger = Logger.getLogger(CsvLoader.class.getName());
     private final LoadDatabase loadDatabase;
-
+    @Value("${csvfile.relativepath}")
+    private String filePath;
     @Override
     public void run(String... args) throws Exception {
         logger.info("CSV Data load started");
-        List<CsvBean> csvBeans = CsvFileReader.readCsvFile("data/movielist.csv");
+        List<CsvBean> csvBeans = CsvFileReader.readCsvFile(filePath);
         if (csvBeans.isEmpty()) {
             logger.info("CSV Data load ended with no data found");
             return;
